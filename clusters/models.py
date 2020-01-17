a4eca543d1b16c67cbe9593c5b6493d2270075e6
CeciliaManzoni <xpeppers@xpepperss-MacBook-Pro.loca>
2020-01-17 15:45:09 +0100

    Add Circle and Topic Models with Test

--- clusters/models.py
+++ clusters/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Circle(models.Model):
+    name = models.CharField(max_length=500)
+
+    def __repr__(self):
+        return f"{self.name} Circle"
+
+    def __str__(self):
+        return f"{self.name} Circle"
+
+class Topic(models.Model):
+    name = models.CharField(max_length=500)
+    circle = models.ForeignKey(Circle, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} Topic"
+
+    class Meta:
+        unique_together = (("name","circle"),)
+
+
--- clusters/tests.py
+++ clusters/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+from .models import Circle, Topic
+
+class TopicModelTest(TestCase):
+
+    def test_unique_constraint_topic_circle(self):
+        circle = Circle(name='testCircle')
+        circle.save()
+
+        topic = Topic(name='testTopic',circle=circle)
+        topic.save()
+
+        with self.assertRaises(IntegrityError):
+            topic_double = Topic(name='testTopic',circle=circle)
+            topic_double.save()
