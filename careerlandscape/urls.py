f408175590e82f0dde402d1668cf7eb7e410bbcf
CeciliaManzoni <xpeppers@xpepperss-MacBook-Pro.loca>
2020-01-21 15:20:16 +0100

    Refactor Index and urls

--- careerlandscape/urls.py
+++ careerlandscape/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("clusters/", include("clusters.urls")),
+    path("", include("clusters.urls")),
 ]
