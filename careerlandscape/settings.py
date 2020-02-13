8dd2c80bf178aa5bdc1cc0444977b9420cadb1f7
CeciliaManzoni <xpeppers@xpepperss-MacBook-Pro.loca>
2020-02-13 15:38:54 +0100

    Modify settings

--- careerlandscape/settings.py
+++ careerlandscape/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJ_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv("DJ_DEBUG")))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
